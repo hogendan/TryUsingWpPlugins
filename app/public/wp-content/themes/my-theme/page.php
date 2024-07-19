21a6aa639b91baeb29dd2ce9c21e352da3b4af84
渡邉友樹 <>
2024-07-19 15:34:02 +0900

    feat: 投稿/固定ページにカスタムフィールド追加

--- app/public/wp-content/themes/my-theme/page.php
+++ app/public/wp-content/themes/my-theme/page.php
@@ -4,8 +4,15 @@
     <h1>
         <?php the_title(); ?>
     </h1>
-    <div>
-    </div>
+    <section>
+        <div>
+            <?php the_field('biography'); ?>
+        </div>
+        <div>
+            <?php the_field('guide_start_date'); ?>
+            <?php the_field('guide_end_date'); ?>
+        </div>
+    </section>
     <?php the_content(); ?>
 </main>
 <?php get_footer(); ?>
--- app/public/wp-content/themes/my-theme/single.php
+++ app/public/wp-content/themes/my-theme/single.php
@@ -3,8 +3,15 @@
 <main>
     <p><?php echo get_the_date(); ?></p>
     <h1><?php the_title(); ?></h1>
-    <div>
-    </div>
+    <section>
+        <div>
+            <?php the_field('biography'); ?>
+        </div>
+        <div>
+            <?php the_field('guide_start_date'); ?>
+            <?php the_field('guide_end_date'); ?>
+        </div>
+    </section>
     <?php the_content(); ?>
 </main>
 <?php get_footer(); ?>
